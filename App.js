b71c49b9313fc143309b1efcba3e0281dbaa5d22
abhinav <bhattabhinav@github.com>
2021-03-15 15:57:59 -0400

    topbar component created

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants';
+import TopBar from './components/TopBar'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TopBar/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight
   },
 });

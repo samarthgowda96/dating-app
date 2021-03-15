0075d3780b83d2e1b53143ed92e8723e7429b2e9
abhinav <bhattabhinav@github.com>
2021-03-15 16:12:55 -0400

    fontawesome icons added

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { View, Text, StyleSheet  } from 'react-native'
+import { FontAwesome5, FontAwesome } from '@expo/vector-icons'
 
 export default function TopBar() {
     return (
-        <View style={style.container}>
-            <Text></Text>
-        </View>
+        <View style={styles.container}>
+        <FontAwesome5 name="fire" size={27} color="#F06795" />
+        <FontAwesome name="comments" size={27} color="#5c5c5c" />
+        <FontAwesome name="user" size={27} color="#5c5c5c" />
+      </View>
     )
 }
 

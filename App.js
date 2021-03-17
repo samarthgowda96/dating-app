3d8c258914dfe77e101b094e974e1a537df79162
abhinav <bhattabhinav@github.com>
2021-03-17 11:58:42 -0400

    ui prototype version 1 completed

--- App.js
+++ App.js
@@ -1,40 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect, useRef } from 'react'
-import { StyleSheet, View, Alert } from 'react-native';
-import Constants from 'expo-constants';
-import TopBar from './components/TopBar'; 
+import { StyleSheet, View, Alert } from 'react-native'
+import Constants from 'expo-constants'
+import TopBar from './components/TopBar'
 import axios from 'axios'
+import BottomBar from './components/BottomBar'
+import Swipes from './components/Swipes'
 
 export default function App() {
+  const [users, setUsers] = useState([])
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const swipesRef = useRef(null)
 
-  const [user, setUser] = useState([]);
-  const fetchUser = async () => {
-try {
-  const {data} = await axios.get('https://randomuser.me/api/?gender=female&results=50')
-  setUser(data.results)
-} catch (error) {
-  console.log(error)
-  Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }])
-}
+  async function fetchUsers() {
+    try {
+      const { data } = await axios.get('https://randomuser.me/api/?gender=female&results=50')
+      setUsers(data.results)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }])
+    }
   }
 
-  useEffect(()=> {
-      fetchUser();
+  useEffect(() => {
+    fetchUsers()
   }, [])
+
+  function handleLike() {
+    console.log('like')
+    nextUser()
+  }
+
+  function handlePass() {
+    console.log('pass')
+    nextUser()
+  }
+
+  function nextUser() {
+    const nextIndex = users.length - 2 === currentIndex ? 0 : currentIndex + 1
+    setCurrentIndex(nextIndex)
+  }
+
+  function handleLikePress() {
+    swipesRef.current.openLeft()
+  }
+  function handlePassPress() {
+    swipesRef.current.openRight()
+  }
+
   return (
-    
     <View style={styles.container}>
-      {console.log(user)}
-      <TopBar/>
-      <View style = {styles.swipes}></View>
+      <TopBar />
+      <View style={styles.swipes}>
+        {users.length > 1 &&
+          users.map(
+            (u, i) =>
+              currentIndex === i && (
+                <Swipes
+                  key={i}
+                  ref={swipesRef}
+                  currentIndex={currentIndex}
+                  users={users}
+                  handleLike={handleLike}
+                  handlePass={handlePass}
+                ></Swipes>
+              )
+          )}
+      </View>
+      <BottomBar handleLikePress={handleLikePress} handlePassPress={handlePassPress} />
+      {console.log(currentIndex)}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Constants.statusBarHeight
+    marginTop: Constants.statusBarHeight,
   },
   swipes: {
     flex: 1,
@@ -44,9 +85,9 @@ const styles = StyleSheet.create({
     shadowOffset: {
       width: 0,
       height: 3,
-    }, 
+    },
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
     elevation: 7,
-  }
-});
+  },
+})
